8556e233cfcbf438bfed6225c26f04b59a4f731c
Laboratorio 109 Pc-05 <lab-109-inf-05>
2024-01-18 18:22:47 -0400

    Primer commit

---
+++ src/PrimerPrograma.java
@@ -0,0 +1,7 @@
+public class PrimerPrograma {
+    public static void main(String[] args) {
+     String nombre ="Enmanuel";
+     double monto =475.50 ;
+     double resultado= (monto * 0.15)+monto;
+     System.out.println("Mi nombre es  "+ nombre + "El resultado es : "+ resultado );
+}}
